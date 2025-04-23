a18c6b12aa7f3d4bc7ab3a0db14c11f8ff136e79
mayur16129 <54818583+mayur16129@users.noreply.github.com>
2025-04-23 13:13:27 +0530

    Create Assignment-math-operation.py

---
+++ Assignment-math-operation.py
@@ -0,0 +1,12 @@
+Number1=int(input('Enter the Number :'))
+Number2=int(input('Enter the Number :'))
+a=Number1+Number2
+b=Number1-Number2
+c=Number1*Number2
+d=Number1/Number2
+print("Addition :",a)
+print("Subtraction :",b)
+print("Multiplication :",c)
+print("Division :",d)
+
+print('Thanks!')
